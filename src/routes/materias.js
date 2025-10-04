e570e55b1fbd743389567fce5de67597bd65fd9a
LizethDanielaV <lizethdanielavb@ufps.edu.co>
2025-10-04 18:49:23 -0500

    feat(materia): add actualizar materia

--- src/routes/materias.js
+++ src/routes/materias.js
@@ -4,5 +4,5 @@ import MateriaController from "../controllers/MateriaController.js";
 const router = Router();
 
 router.post("/", MateriaController.crear);
-
+router.put("/:id_materia", MateriaController.actualizar);
 export default router;
