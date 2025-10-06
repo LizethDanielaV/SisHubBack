cd97e4e2bf771c38bec4863a996b453fdd0541fe
LeydiD <leydialejaduran@gmail.com>
2025-10-06 09:06:49 -0500

    Cambios en la actualizacion del estado del grupo (deshabilitar)

--- src/routes/grupos.js
+++ src/routes/grupos.js
@@ -4,7 +4,7 @@ import GrupoController from "../controllers/GrupoController.js";
 const router = Router();
 
 router.post("/", GrupoController.crearGrupo);
-router.patch("/:id_grupo/deshabilitar", GrupoController.deshabilitarGrupo);
+router.patch("/:id_grupo/actualizar-estado", GrupoController.actualizarEstado);
 router.get("/generar-clave", GrupoController.generarClaveAcceso);
 router.get("/:id_grupo/generar-qr", GrupoController.generarCodigoQR);
 router.get("/:id_grupo/clave-y-qr", GrupoController.obtenerClaveYCodigoQR);
