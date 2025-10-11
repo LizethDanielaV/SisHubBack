716ed2f639c0501295421014ccb42172704289c4
ElMopri <alopeztriana1@gmail.com>
2025-10-10 22:43:01 -0500

    arreglo del uniser a un grupo con clave

--- src/controllers/GrupoUsuarioController.js
+++ src/controllers/GrupoUsuarioController.js
@@ -1,9 +1,16 @@
 import GrupoUsuarioService from "../services/GrupoUsuarioService.js";
 
 async function unirseAGrupoPorIdYClave(req, res) {
-    const { id_usuario, id_grupo, clave_acceso } = req.body;
+    const { codigo_usuario, codigo_materia, nombre_grupo, periodo, anio, clave_acceso } = req.body;
     try {
-        const resultado = await GrupoUsuarioService.unirseAGrupoPorIdYClave(id_usuario, id_grupo, clave_acceso);
+        const resultado = await GrupoUsuarioService.unirseAGrupoPorIdYClave(
+            codigo_usuario,
+            codigo_materia,
+            nombre_grupo,
+            periodo,
+            anio,
+            clave_acceso
+        );
         res.status(201).json(resultado);
     } catch (error) {
         res.status(400).json({ error: error.message });
