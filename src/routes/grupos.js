1199f4ef7fbd68fe1a227b7128198c542e5daa46
LeydiD <leydialejaduran@gmail.com>
2025-10-12 11:30:20 -0500

    Cambios de la patrona miche

--- src/routes/grupos.js
+++ src/routes/grupos.js
@@ -25,7 +25,7 @@ router.post("/", GrupoController.crearGrupo);
 router.patch("/actualizar-estado", GrupoController.actualizarEstado);
 router.get("/generar-clave", GrupoController.generarClaveAcceso);
 router.get("/:codigo_materia/:nombre/:periodo/:anio/generar-qr", GrupoController.generarCodigoQR);
-router.get("/clave-y-qr", GrupoController.obtenerClaveYCodigoQR);
+router.post("/clave-y-qr", GrupoController.obtenerClaveYCodigoQR);
 router.get("/materia/:codigo_materia", GrupoController.listarGruposPorMateria);
 router.get("/materia/:codigo_materia/habilitados", GrupoController.listarGruposHabilitadosPorMateria);
 router.get("/usuario/:codigo_usuario", GrupoController.listarGruposPorUsuario);
