9e406bfdf656e180dadaec8f60e88bbe90a3b61e
saimersaavedra <saimeradriansr@ufps.edu.co>
2025-10-13 14:48:42 -0500

    feat: add fuction update password in groups

--- src/routes/grupos.js
+++ src/routes/grupos.js
@@ -26,6 +26,7 @@ router.patch("/actualizar-estado", GrupoController.actualizarEstado);
 router.get("/generar-clave", GrupoController.generarClaveAcceso);
 router.get("/:codigo_materia/:nombre/:periodo/:anio/generar-qr", GrupoController.generarCodigoQR);
 router.post("/clave-y-qr", GrupoController.obtenerClaveYCodigoQR);
+router.patch("/actualizar-clave", GrupoController.actualizarClaveAcceso);
 router.get("/materia/:codigo_materia", GrupoController.listarGruposPorMateria);
 router.get("/materia/:codigo_materia/habilitados", GrupoController.listarGruposHabilitadosPorMateria);
 router.get("/usuario/:codigo_usuario", GrupoController.listarGruposPorUsuario);
