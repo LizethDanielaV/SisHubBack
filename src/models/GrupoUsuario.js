d29108700597f29213b078a1f8331b20d44ff46b
LeydiD <leydialejaduran@gmail.com>
2025-10-18 17:35:52 -0500

    Se agregó estado a la clase GrupoUsuario

--- src/models/GrupoUsuario.js
+++ src/models/GrupoUsuario.js
@@ -4,25 +4,26 @@ import Usuario from "./Usuario.js";
 import Grupo from "./Grupo.js";
 
 const GrupoUsuario = db.define("grupo_usuario", {
-    id_grupo_usuario: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    fecha_ingreso: { type: DataTypes.DATEONLY, defaultValue: DataTypes.NOW },
+  id_grupo_usuario: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  fecha_ingreso: { type: DataTypes.DATEONLY, defaultValue: DataTypes.NOW },
 
-    // Claves foráneas hacia Grupo (compuesta)
-    codigo_materia: { type: DataTypes.STRING(20), allowNull: false },
-    nombre: { type: DataTypes.STRING(1), allowNull: false },
-    periodo: { type: DataTypes.STRING(2), allowNull: false },
-    anio: { type: DataTypes.INTEGER, allowNull: false },
+  // Claves foráneas hacia Grupo (compuesta)
+  codigo_materia: { type: DataTypes.STRING(20), allowNull: false },
+  nombre: { type: DataTypes.STRING(1), allowNull: false },
+  periodo: { type: DataTypes.STRING(2), allowNull: false },
+  anio: { type: DataTypes.INTEGER, allowNull: false },
+  estado: { type: DataTypes.BOOLEAN, defaultValue: false },
 }, {
-    timestamps: false,
-    freezeTableName: true
+  timestamps: false,
+  freezeTableName: true
 });
 
 //relacion con Usuario
 Usuario.hasMany(GrupoUsuario, {
-    foreignKey: "codigo_usuario"
+  foreignKey: "codigo_usuario"
 });
 GrupoUsuario.belongsTo(Usuario, {
-    foreignKey: "codigo_usuario"
+  foreignKey: "codigo_usuario"
 });
 
 // Relación con Grupo (clave compuesta)
