ffd3b8335e14a0749869aadb506c35d8f28d2da6
LeydiD <leydialejaduran@gmail.com>
2025-10-30 22:17:32 -0500

    Correcion en la relacion de equipo con proyecto

--- src/models/TipoAlcance.js
+++ src/models/TipoAlcance.js
@@ -1,6 +1,5 @@
 import db from "../db/db.js";
 import { DataTypes } from "sequelize";
-import Esquema from "./Esquema.js";
 
 const TipoAlcance = db.define("Tipo_alcance",{
     id_tipo_alcance: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
