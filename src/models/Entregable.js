787c5a6599566f7f9841d2f09706995567cac254
LizethDanielaV <lizethdanielavb@ufps.edu.co>
2025-10-30 22:19:46 -0500

    Entregable actualizado

--- src/models/Entregable.js
+++ src/models/Entregable.js
@@ -1,16 +1,17 @@
 import db from "../db/db.js";
 import { DataTypes } from "sequelize";
 import Proyecto from "./Proyecto.js";
+import Equipo from "./Equipo.js";
+import Actividad from "./Actividad.js";
 
 const Entregable = db.define("entregable", {
     id_entregable: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
     tipo: {type: DataTypes.STRING(100), allowNull: false},
     nombre_archivo: {type: DataTypes.STRING(200)},
     url_archivo: { type: DataTypes.STRING(255)},
-    decripcion: {type: DataTypes.STRING(200)},
+    comentarios: {type: DataTypes.STRING(200)},
     fecha_subida: {type: DataTypes.DATEONLY, defaultValue: DataTypes.NOW },
-    extension: {type: DataTypes.STRING(10)}, 
-    tamaño:{ type: DataTypes.BIGINT}
+    calificacion: {type: DataTypes.DECIMAL(4, 2)}
 }, {
      timestamps: false,
     freezeTableName: true
@@ -23,5 +24,21 @@ Proyecto.hasMany(Entregable, {
 Entregable.belongsTo(Proyecto, {
     foreignKey: "id_proyecto"
 });
+
+//relacion con equipo
+Equipo.hasMany(Entregable, {
+    foreignKey: "id_equipo"
+});
+Entregable.belongsTo(Equipo, {
+     foreignKey: "id_equipo"
+});
+
+//Relacion con actividad
+Actividad.hasMany(Entregable, {
+    foreignKey: "id_actividad"
+})
+Entregable.belongsTo(Actividad, {
+    foreignKey: "id_actividad"
+})
 
 export default Entregable;
