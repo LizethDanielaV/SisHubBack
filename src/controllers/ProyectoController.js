a8770fe53757476a1d8b3f7d33005ffa11147a58
LizethDanielaV <lizethdanielavb@ufps.edu.co>
2025-11-01 13:29:07 -0500

    Arreglo modelo HitorialProyecto y endpoint listar proyectos director

--- src/controllers/ProyectoController.js
+++ src/controllers/ProyectoController.js
@@ -201,9 +201,21 @@ async function actualizarProyecto(req, res) {
     }
 }
 
+async function listarParaDirector(req, res) {
+  try {
+    const proyectos = await ProyectoService.listarProyectosDirector();
+    res.json(proyectos);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error al obtener materias", error: error.message });
+  }
+}
+
 export default {
     crearProyectoDesdeIdea,
     obtenerProyecto,
     listarProyectosPorGrupo,
-    actualizarProyecto
+    actualizarProyecto, 
+    listarParaDirector
 };
